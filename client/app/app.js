a56bfd99ce8584c6aac01dbc755399de4fd15163
Prasanna Kumar <jpkbeddu@gmail.com>
2016-04-07 18:44:20 +0530

    Adding more dependencies, file and url loaders for serving fonts

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,20 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import ngMaterial from 'angular-material';
+import ngAnimate from 'angular-animate';
+import ngCookies from 'angular-cookies';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
 import 'normalize.css';
+import 'angular-material/angular-material.css';
+import 'font-awesome/css/font-awesome.css';
 
 angular.module('app', [
     uiRouter,
+    ngMaterial,
+    ngAnimate,
+    ngCookies,
     Common.name,
     Components.name
   ])
